3418f69b260f3d892c0c5a351e263eeb4a3b8584
Lukasowy <lukasowy@gmail.com>
2018-11-21 20:13:13 +0100

    stack

---
+++ Data_Structure/src/stack/Stack.java
@@ -0,0 +1,54 @@
+package stack;
+
+import java.util.EmptyStackException;
+
+public class Stack<T> {
+    private static class Node<T> {
+        private T data;
+        private Node<T> next;
+
+        Node(T data) {
+            this.data = data;
+        }
+    }
+    private int count = 0;
+    private Node<T> top = null;
+
+    public T pop(){
+        if(top == null) throw new EmptyStackException();
+        T item = top.data;
+        top = top.next;
+        count--;
+
+        return item;
+    }
+
+    public void push(T data){
+        Node<T> node = new Node<>(data);
+        node.next = top;
+        top = node;
+        count++;
+    }
+
+    public T peek(){
+        return top.data;
+    }
+
+    public boolean isEmpty(){
+        return top == null;
+    }
+
+    public int length(){
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(30);
+        System.out.println(stack.top.data);
+        stack.push(30);
+        stack.push(30);
+        stack.pop();
+        System.out.println(stack.length());
+    }
+}
